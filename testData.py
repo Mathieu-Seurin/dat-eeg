584ba8f612be963ba3bc1a3383cdf8c07b3cd4a4
Mathieu-Seurin <matseurin@cegetel.net>
2016-02-15 15:22:36 +0100

    Working with new dataset

--- testData.py
+++ testData.py
@@ -13,7 +13,22 @@ def savePartOfData(filenameI, filenameO):
     np.save(filenameO, miniData)
     np.save('BCI/yFile', y)
 
-data = sio.loadmat("BCI/Subject_A_Train_reshaped.mat")
+def reformatRawData(filenameI):
+    data = sio.loadmat(filenameI)
+    data = np.array(data['X'])
+
+    newData = []
+
+    for exemple in np.size(data,3):
+        newData.append(np.concatenate( (data[i,exemple,:] for i in range(64))))
+
+    print( newData[5][:160]) )
+    print( data[0,:,5] )
+
+
+
+
+
 
 # savePartOfData("BCI/Subject_A_Train_reshaped.mat", "BCI/quickTest")
 
